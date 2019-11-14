03d15830afe01ca33e81e748371827e1b011c15d
Nicholas Chedid <Nicholas@Nicholass-MacBook-Pro.local>
2019-11-14 10:35:18 +0000

    added reset password page

---
+++ src/views/ResetPw.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <h3>reset pw page</h3>
+    <div class="row">
+      <form @submit.prevent="sendEmail()" class="col s12">
+        <div class="row">
+          <div class="input-field col s12">
+            <input type="text" v-model="email" required />
+            <label>Email</label>
+          </div>
+        </div>
+        <button type="submit" class="btn">Submit</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import firebase from "firebase/app";
+export default {
+  data() {
+    return {
+      email: ""
+    };
+  },
+  methods: {
+    sendEmail() {
+      firebase
+        .auth()
+        .sendPasswordResetEmail(this.email)
+        .then(() => {
+          console.log(`Email sent to ${this.email}`);
+        })
+        .catch(err => console.log(err));
+    }
+  }
+};
+</script>
+
+<style>
+</style>
