8240caf24eacb263a3190b79d047329f6f80d783
Nicholas Chedid <Nicholas@Nicholass-MacBook-Pro.local>
2019-11-14 15:43:58 +0000

    Pointlog added

---
+++ src/views/PointLog.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h3>Point Log</h3>
+    <p
+      v-for="log in logs"
+      :key="log.id"
+    >{{log.name}} got {{log.pointField}} point @ {{log.time.toDate() | formatDate}}</p>
+  </div>
+</template>
+
+<script>
+import moment from "moment";
+import { db } from "../data/firebaseInit";
+import firebase from "firebase/app";
+export default {
+  data() {
+    return {
+      logs: []
+    };
+  },
+  filters: {
+    formatDate: function(value) {
+      if (value) {
+        return moment(value).format("DD/MMM/YYYY HH:mm");
+      }
+    }
+  },
+  created() {
+    db.collection("pointLog")
+      .orderBy("time", "desc")
+      .limit(25)
+      .get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          // console.log(doc.data());
+          const data = {
+            id: doc.id,
+            name: doc.data().name,
+            pointField: doc.data().pointField,
+            time: doc.data().time
+          };
+          this.logs.push(data);
+        });
+      });
+  }
+};
+</script>
+
+<style>
+</style>
