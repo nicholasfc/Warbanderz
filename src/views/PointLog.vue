7c5ffd322654063006094e5635bdaaa32f062760
Nicholas Chedid <Nicholas@Nicholass-MacBook-Pro.local>
2019-11-15 16:00:01 +0000

    Added if add or remove a point

--- src/views/PointLog.vue
+++ src/views/PointLog.vue
@@ -4,7 +4,7 @@
     <p
       v-for="log in logs"
       :key="log.id"
-    >{{log.name}} got {{log.pointField}} point @ {{log.time.toDate() | formatDate}}</p>
+    >{{log.name}} got {{log.pointField}} point {{log.addRemove}} @ {{log.time.toDate() | formatDate}}</p>
   </div>
 </template>
 
@@ -32,12 +32,12 @@ export default {
       .get()
       .then(querySnapshot => {
         querySnapshot.forEach(doc => {
-          // console.log(doc.data());
           const data = {
             id: doc.id,
             name: doc.data().name,
             pointField: doc.data().pointField,
-            time: doc.data().time
+            time: doc.data().time,
+            addRemove: doc.data().addRemove
           };
           this.logs.push(data);
         });
