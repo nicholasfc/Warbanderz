9cd05b1ce9b3ae595ccb0096f4e9e0a9b77fa5d3
Nicholas Chedid <Nicholas@Nicholass-MacBook-Pro.local>
2019-11-18 17:31:15 +0000

    Added vuetify to home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,26 @@
 <template>
   <div class="home">
-    <h3>Welcome to Site</h3>
-    <template v-if="isAdmin">
-      <h3>Add user to admin</h3>
-      <div class="row">
-        <form @submit.prevent="addAdmin()" class="col s12">
-          <div class="row">
-            <div class="input-field col s12">
-              <input type="email" v-model="email" required />
-              <label>Email</label>
-            </div>
-          </div>
-          <button type="submit" class="btn">Submit</button>
-          <router-link to="/members" class="btn grey">Cancel</router-link>
-        </form>
-      </div>
-    </template>
+    <h3 class="font-weight-medium text-center pa-3 ma-3 display-1">Welcome to Warbanderz</h3>
+    <v-divider></v-divider>
+    <v-content v-if="isAdmin">
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="4">
+            <v-card>
+              <v-toolbar flat>
+                <v-toolbar-title>Add Admin Role</v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field required label="Player Name" v-model="email"></v-text-field>
+                  <v-btn text class="success" @click="addAdmin()">Add Admin</v-btn>
+                </v-form>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
   </div>
 </template>
 
