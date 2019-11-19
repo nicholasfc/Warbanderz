eabff715d1f8a432bc7d58e7cb5bcdadf9555c52
Nicholas Chedid <Nicholas@Nicholass-MacBook-Pro.local>
2019-11-19 15:30:42 +0000

    removed commented code that wasn't need anymore

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -30,33 +30,6 @@
         <router-link to="/resetpw">Forgot Password?</router-link>
       </p>
     </v-content>
-
-    <!-- <div class="container">
-      <div class="row">
-        <div class="col s12 m8 offset-m2">
-          <div class="login card-panel green white-text center">
-            <h3>Login</h3>
-            <form action="index.html">
-              <div class="input-field">
-                <i class="material-icons prefix">email</i>
-                <input type="email" id="email" v-model="email" />
-                <label class="white-text" for="email">Email Address</label>
-              </div>
-              <div class="input-field">
-                <i class="material-icons prefix">lock</i>
-                <input type="password" id="password" v-model="password" />
-                <label class="white-text" for="password">Password</label>
-              </div>
-              <button
-                @click.prevent="login()"
-                class="btn btn-large btn-extended grey lighten-4 black-text"
-              >Login</button>
-            </form>
-          </div>
-        </div>
-      </div>
-      <router-link to="/resetpw">Forgot Password?</router-link>
-    </div>-->
   </div>
 </template>
 
