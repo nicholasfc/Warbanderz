2396965a70773edaa70b27d8f40c2c924b00f4cf
Nicholas Chedid <Nicholas@Nicholass-MacBook-Pro.local>
2019-11-19 14:25:01 +0000

    Changed navbar from drawer to menu list

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,70 +2,56 @@
   <div class="navbar">
     <nav>
       <v-toolbar text dark>
-        <v-app-bar-nav-icon @click.stop="drawer = !drawer" v-if="isLoggedIn"></v-app-bar-nav-icon>
-        <v-toolbar-title>
+        <v-toolbar-title class="hidden-md-and-down">
           <router-link to="/" class="text-uppercase link">Warbanderz</router-link>
         </v-toolbar-title>
+        <v-toolbar-title class="hidden-md-and-up">
+          <router-link to="/" class="text-uppercase link">Wbz</router-link>
+        </v-toolbar-title>
         <v-spacer></v-spacer>
-        <v-btn text class="primary" @click="logout()">
-          <span>Sign Out</span>
-          <v-icon right>mdi-logout</v-icon>
-        </v-btn>
+
+        <!-- dropdown menu -->
+        <v-toolbar-items>
+          <v-menu offset-y v-if="isLoggedIn">
+            <template v-slot:activator="{ on }">
+              <v-btn text dark v-on="on" class>
+                <v-icon left>mdi-menu-down</v-icon>Menu
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item link href="/member" v-if="isLoggedIn">
+                <v-list-item-content>
+                  <v-list-item-title>Member List</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item link href="/add" v-if="isLoggedIn && isAdmin">
+                <v-list-item-content>
+                  <v-list-item-title>Add Member</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item link href="/log" v-if="isLoggedIn">
+                <v-list-item-content>
+                  <v-list-item-title>Points Log</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item link href="/removed" v-if="isLoggedIn && isAdmin">
+                <v-list-item-content>
+                  <v-list-item-title>Removed Members</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </v-toolbar-items>
+
+        <v-divider vertical v-if="isLoggedIn"></v-divider>
+        <v-toolbar-items>
+          <v-btn text class="primary" v-if="isLoggedIn" @click="logout()">
+            <span>Sign Out</span>
+            <v-icon right>mdi-logout</v-icon>
+          </v-btn>
+        </v-toolbar-items>
       </v-toolbar>
-      <v-navigation-drawer v-model="drawer" absolute temporary dark>
-        <v-list dense>
-          <v-list-item link href="/member" v-if="isLoggedIn">
-            <v-list-item-content>
-              <v-list-item-title>Member List</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item link href="/add" v-if="isLoggedIn && isAdmin">
-            <v-list-item-content>
-              <v-list-item-title>Add Member</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item link href="/log" v-if="isLoggedIn">
-            <v-list-item-content>
-              <v-list-item-title>Points Log</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item link href="/removed" v-if="isLoggedIn && isAdmin">
-            <v-list-item-content>
-              <v-list-item-title>Removed Members</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-navigation-drawer>
     </nav>
-
-    <!-- <v-toolbar dark dense flat>
-      <v-toolbar-title>
-        <router-link to="/">Warbanderz</router-link>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-
-      <v-toolbar-items>
-        <v-btn text v-if="isLoggedIn">
-          <router-link to="/member">Member list</router-link>
-        </v-btn>
-        <v-btn text v-if="isLoggedIn && isAdmin">
-          <router-link to="/add">Add Player</router-link>
-        </v-btn>
-        <v-btn text v-if="isLoggedIn && isAdmin">
-          <router-link to="/log">Points Log</router-link>
-        </v-btn>
-        <v-btn text v-if="isLoggedIn && isAdmin">
-          <router-link to="/removed">Removed Log</router-link>
-        </v-btn>
-        <v-btn text v-if="!isLoggedIn">
-          <router-link to="/login">Login</router-link>
-        </v-btn>
-        <v-btn text v-if="!isLoggedIn">
-          <router-link to="/register">Register</router-link>
-        </v-btn>
-        <v-btn color="red" tile depressed v-if="isLoggedIn" @click="logout()">Logout</v-btn>
-      </v-toolbar-items>
-    </v-toolbar>-->
   </div>
 </template>
 
@@ -76,8 +62,7 @@ export default {
   data() {
     return {
       isLoggedIn: false,
-      isAdmin: false,
-      drawer: null
+      isAdmin: false
     };
   },
   created() {
@@ -106,23 +91,5 @@ export default {
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-/* h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-} */
-.link {
-  text-decoration: none;
-}
 </style>
