7287d2ab8ee3d908782301ca0828199c7ecb6d9c
Nicholas Chedid <Nicholas@Nicholass-MacBook-Pro.local>
2019-12-30 11:11:33 +0000

    Added a search field

--- src/views/PointLog.vue
+++ src/views/PointLog.vue
@@ -16,6 +16,35 @@
         </v-col>
       </v-row>
     </v-content>
+    <v-container>
+      <v-row align="center" justify="center">
+        <v-col cols="12" sm="8" md="4">
+          <v-toolbar-title align="center">Search Points</v-toolbar-title>
+          <v-card-text>
+            <v-form>
+              <v-text-field label="Player Name" v-model="name" required></v-text-field>
+              <v-btn text small class="success mr-5" @click="searchPlayer()">Search Player</v-btn>
+            </v-form>
+          </v-card-text>
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-container>
+      <v-row align="center" justify="center">
+        <v-col cols="12" sm="8" md="4" lg="12">
+          <v-card-text align="center">
+            <p v-for="log in searched" :key="log.id" class="body-2">
+              <span class="font-weight-bold">{{log.name}}</span>
+              got
+              <span class="font-weight-bold">{{log.pointField}}</span> point
+              <span class="font-weight-bold">{{log.addRemove}}</span>
+              @ {{log.time.toDate() | formatDate}} by {{log.byWho}}
+            </p>
+          </v-card-text>
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-content></v-content>
     <v-content v-if="!isAdmin && !isMember">
       <p
         class="font-weight-medium text-center pa-3 ma-3 display-1"
@@ -33,7 +62,9 @@ export default {
     return {
       logs: [],
       isMember: false,
-      isAdmin: false
+      isAdmin: false,
+      searched: [],
+      name
     };
   },
   filters: {
@@ -71,6 +102,28 @@ export default {
           this.logs.push(data);
         });
       });
+  },
+  methods: {
+    searchPlayer() {
+      db.collection("pointLog")
+        .where("name", "==", this.name)
+        .orderBy("time", "desc")
+        .limit(25)
+        .get()
+        .then(querySnapshot => {
+          querySnapshot.forEach(doc => {
+            const data = {
+              id: doc.id,
+              name: doc.data().name,
+              pointField: doc.data().pointField,
+              time: doc.data().time,
+              addRemove: doc.data().addRemove,
+              byWho: doc.data().byWho
+            };
+            this.searched.push(data);
+          });
+        });
+    }
   }
 };
 </script>
@@ -78,6 +131,6 @@ export default {
 <style scoped>
 h3 {
   text-decoration: underline;
-  color: #37474f;
+  color: #eceff1;
 }
 </style>
