f22741d25e358c782505b084df91d8bb317b2ad0
Nicholas Chedid <Nicholas@Nicholass-MacBook-Pro.local>
2020-01-22 15:34:26 +0000

    Remove add player route

--- src/router/index.js
+++ src/router/index.js
@@ -16,97 +16,6 @@ import firebase from "firebase/app";
 
 Vue.use(VueRouter);
 
-// const routes = [
-//   {
-//     path: "/",
-//     name: "home",
-//     component: Home,
-//     meta: {
-//       requiresAuth: true
-//     }
-//   },
-//   {
-//     path: "/login",
-//     name: "login",
-//     component: Login,
-//     meta: {
-//       requiresGuest: true
-//     }
-//   },
-//   {
-//     path: "/register",
-//     name: "register",
-//     component: Register,
-//     meta: {
-//       requiresGuest: true
-//     }
-//   },
-//   {
-//     path: "/resetpw",
-//     name: "resetpw",
-//     component: ResetPw,
-//     meta: {
-//       requiresGuest: true
-//     }
-//   },
-//   {
-//     path: "/member",
-//     name: "memberlist",
-//     component: Memberlist,
-//     meta: {
-//       requiresAuth: true
-//     }
-//   },
-//   {
-//     path: "/removed",
-//     name: "removedlog",
-//     component: RemovedLog,
-//     meta: {
-//       requiresAuth: true
-//     }
-//   },
-//   {
-//     path: "/log",
-//     name: "pointlog",
-//     component: PointLog,
-//     meta: {
-//       requiresAuth: true
-//     }
-//   },
-//   {
-//     path: "/add",
-//     name: "addplayer",
-//     component: AddPlayer,
-//     meta: {
-//       requiresAuth: true
-//     }
-//   },
-//   {
-//     path: "/:name",
-//     name: "member",
-//     component: Member,
-//     meta: {
-//       requiresAuth: true
-//     }
-//   },
-//   {
-//     path: "/edit/:name",
-//     name: "edit",
-//     component: EditPlayer,
-//     meta: {
-//       requiresAuth: true
-//     }
-//   },
-//   {
-//     path: "/delete/:name",
-//     name: "delete",
-//     component: DeletePlayer,
-//     meta: {
-//       requiresAuth: true
-//     }
-//   }
-// ];
-
 const routes = [
   {
     path: "/",
@@ -155,14 +64,14 @@ const routes = [
       requiresAuth: true
     }
   },
-  {
-    path: "/add",
-    name: "addplayer",
-    component: AddPlayer,
-    meta: {
-      requiresAuth: true
-    }
-  },
+  // {
+  //   path: "/add",
+  //   name: "addplayer",
+  //   component: AddPlayer,
+  //   meta: {
+  //     requiresAuth: true
+  //   }
+  // },
   {
     path: "/:name",
     name: "member",
@@ -203,46 +112,9 @@ const router = new VueRouter({
   routes
 });
 
-// router.beforeEach((to, from, next) => {
-//   //check for requiredauth guard
-//   if (to.matched.some(record => record.meta.requiresAuth)) {
-//     //check if not logged in
-//     if (!firebase.auth().currentUser) {
-//       //go to login
-//       next({
-//         path: "/login",
-//         query: {
-//           redirect: to.fullPath
-//         }
-//       });
-//     } else {
-//       //procced to route
-//       next();
-//     }
-//   } else if (to.matched.some(record => record.meta.requiresGuest)) {
-//     if (to.matched.some(record => record.meta.requiresAuth)) {
-//       //check if logged in
-//       if (firebase.auth().currentUser) {
-//         //go to login
-//         next({
-//           path: "/",
-//           query: {
-//             redirect: to.fullPath
-//           }
-//         });
-//       } else {
-//         //procced to route
-//         next();
-//       }
-//     } else {
-//       next();
-//     }
-//   }
-// });
 router.beforeEach((to, from, next) => {
   const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
   const isAuthenticated = firebase.auth().currentUser;
-  // console.log("isauthenticated", isAuthenticated);
   if (requiresAuth && !isAuthenticated) {
     next("/login");
   } else {
